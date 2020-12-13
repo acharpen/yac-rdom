47a2f986a5a6a4db2ae45374af3d4c1e64a84f08
Alan Charpentier <alan.charpentier@gmail.com>
2020-12-13 20:55:21 +0100

    update example

--- examples/example.ts
+++ examples/example.ts
@@ -1,21 +1,38 @@
 import { h, render } from '../src/h';
-import { createState } from '../src/r';
+import { R } from '../src/r';
 
-const counter = createState(73);
+const counter = R.state(73);
+const counterClass = R.state('bold');
 
-const counterClass = createState('bold');
+const userFirstName = R.state('Dupont');
+const userFirstNameFirstLetter = R.on([userFirstName], () => userFirstName.get()[0]);
+const userLastName = R.state('Dupond');
+const userLastNameFirstLetter = R.on([userLastName], () => userLastName.get()[0]);
 
 render(
   h(
     'div',
     //
     h('h1', 'Test'),
-    h('p', { class: counterClass, 'data-id': 'counter', style: 'color: red' }, counter)
+    h('p', { class: counterClass, style: 'color: red' }, counter),
+    h(
+      'div',
+      //
+      h('div', userFirstNameFirstLetter, userLastNameFirstLetter),
+      h('div', userFirstName),
+      h('div', userLastName)
+    )
   ),
   document.body
 );
 
-setTimeout(() => {
+setInterval(() => {
   counter.set(counter.get() + 1);
+}, 1000);
+
+setTimeout(() => {
   counterClass.set([counterClass.get(), 'italic'].join(' '));
+
+  userFirstName.set('Capitaine');
+  userLastName.set('Haddock');
 }, 1000);
