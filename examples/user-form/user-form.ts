08731c8803aaa63b513a8a81fcf0d17341053960
Alan Charpentier <alan.charpentier@gmail.com>
2020-12-28 16:02:40 +0100

    update examples

--- examples/user-form/user-form.ts
+++ examples/user-form/user-form.ts
@@ -1,28 +1,29 @@
+import { isNullOrUndefined } from 'util';
 import { h } from '../../src/h';
 import { R } from '../../src/r';
 
 export const UserForm = () => {
   const firstName = R.state('Dupont');
-  const firstNameFirstLetter = R.on([firstName], () => firstName.get()[0]);
   const lastName = R.state('Dupond');
-  const lastNameFirstLetter = R.on([lastName], () => lastName.get()[0]);
+  const initials = R.on([firstName, lastName], () =>
+    [firstName, lastName]
+      .map((state) => state.get()[0])
+      .filter(Boolean)
+      .join('.')
+  );
 
   const handleChangeFirstName = (event: Event): void => {
-    firstName.set((event.target as HTMLInputElement).value);
+    firstName.set((event.target as HTMLInputElement).value ?? '');
   };
 
   const handleChangeLastName = (event: Event): void => {
-    lastName.set((event.target as HTMLInputElement).value);
+    lastName.set((event.target as HTMLInputElement).value ?? '');
   };
 
   return h(
     'div',
     { class: 'user' },
-    h(
-      'div',
-      h('span', { class: 'material-icons' }, 'account_circle'),
-      h('div', { class: 'initials' }, firstNameFirstLetter, '.', lastNameFirstLetter, '.')
-    ),
+    h('div', h('span', { class: 'material-icons' }, 'account_circle'), h('div', { class: 'initials' }, initials)),
     h('div', h('div', 'First'), h('input', { value: firstName, change: handleChangeFirstName })),
     h('div', h('div', 'Last'), h('input', { value: lastName, change: handleChangeLastName }))
   );
